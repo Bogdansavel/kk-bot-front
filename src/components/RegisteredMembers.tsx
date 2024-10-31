9f005e38e514bed8a52b80b54ff17a34ddb4567e
bogdansavel <bogdanarsaman@gmail.com>
2024-11-01 00:02:22 +0100

    add any syntaxis to new members page

--- src/components/RegisteredMembers.tsx
+++ src/components/RegisteredMembers.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function RegisteredMembers() {
     const max = 15;
 
-    const [members, setMembers] = useState([])
+    const [members, setMembers] = useState<any>([])
 
     useEffect(() => {
         fetch("http://localhost:8080/members")
@@ -16,7 +16,7 @@ function RegisteredMembers() {
         <>
             <div className="justify-center items-center">
                 <div className="flex justify-center text-xl pb-2">{members.length}/{max}</div>
-                {members.map((member) => {
+                {members.map((member: any) => {
                     return (
                         <div className="flex justify-center" key={member.id}>
                             <div className="m-2 p-2 bg-slate-400 border-solid rounded-2xl border-4 border-sky-500" key={member.id}><a href={"t.me/${member.userName}"}>@{member.userName}</a></div>
