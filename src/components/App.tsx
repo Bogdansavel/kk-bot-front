8b7a51469a3e6e90605df166a487dc5f6affd867
bogdansavel <bogdanarsaman@gmail.com>
2024-11-25 18:31:55 +0100

    new version

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,3 @@
-import RegistrationForm from './RegistrationForm'
 import Rate from './Rate';
 import RegisteredMembers from './RegisteredMembers';
 import { Routes, Route } from "react-router-dom";
@@ -7,7 +6,6 @@ import Rates from './Rates';
 function App() {
   return (
     <Routes>
-      <Route path="/registration" element={<RegistrationForm />} />
       <Route path="/rate/:movieId" element={<Rate />} />
       <Route path="/rates/:movieId" element={<Rates />} />
       <Route path="/members" element={<RegisteredMembers />} />
