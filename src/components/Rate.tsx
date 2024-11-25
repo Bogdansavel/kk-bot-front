aaeaf96fcd2b3e6deacfc1e08f37dd5db2676b7a
bogdansavel <bogdanarsaman@gmail.com>
2024-11-25 18:43:04 +0100

    new version

--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -12,8 +12,8 @@ import { BASE_URL } from '../Constants';
 import { useParams } from 'react-router-dom';
 
 function Rate() {
-    const {movieId} = useParams();
-    const {webApp, executeMethod} = useTelegram()
+    const { movieId } = useParams();
+    const { webApp, executeMethod } = useTelegram()
     const { id, username, first_name } = webApp.initDataUnsafe?.user
     //const username = "test";
     //const first_name = "test";
